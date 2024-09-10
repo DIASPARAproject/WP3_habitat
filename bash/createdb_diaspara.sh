8aebfd83cb5c5217e5fa8bbd448e633130f99715
Jules Oliviero <jules.oliviero@inrae.fr>
2024-09-10 14:42:47 +0200

    dev : restoring all dumps with pg_restore

--- bash/createdb_diaspara.sh
+++ bash/createdb_diaspara.sh
@@ -27,3 +27,16 @@ pg_dump --table ccm21.namedrivers -Fc -f namedrivers.dump --dbname postgresql://
 pg_dump --table ccm21.rivernodes -Fc -f rivernodes.dump --dbname postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/eda2.0 |psql --dbname postgresql://${env:userjules}:${env:passjules}@${env:hostdiaspara}/diaspara
 pg_dump --table ccm21.riversegments -Fc -f riversegments.dump --dbname postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/eda2.0 |psql --dbname postgresql://${env:userjules}:${env:passjules}@${env:hostdiaspara}/diaspara
 pg_dump --table ccm21.seaoutlets -Fc -f seaoutlets.dump --dbname postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/eda2.0 |psql --dbname postgresql://${env:userjules}:${env:passjules}@${env:hostdiaspara}/diaspara
+
+# restore tables to schema ccm
+psql -U postgres -c "alter schema ccm rename to ccm21" diaspara
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 catchments.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 coast.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 islands.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 lakes.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 mainrivers.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 namedrivers.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 rivernodes.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 riversegments.dump
+pg_restore -v --dbname=postgresql://${env:userjules}:${env:passjules}@$env:hostdiaspara/diaspara -n ccm21 seaoutlets.dump
+psql -U postgres -c "alter schema ccm21 rename to ccm" diaspara
