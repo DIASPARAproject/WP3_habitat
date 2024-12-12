a407f4d36fd5c9880da711da040927c82f7a9aa5
olivierojules <oliviero.jules@gmail.com>
2024-12-12 15:47:30 +0100

    fix: no more trouble while importing ices layers

--- bash/1_osgeo4w_ogr2ogr_gdb_postgres_ices.bash
+++ bash/1_osgeo4w_ogr2ogr_gdb_postgres_ices.bash
@@ -1,5 +1,5 @@
 # launch in OSGEO4W console, (QGIS) line by line
 
-ogr2ogr.exe -f "PostgreSQL" -a_srs EPSG:4326 PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="ices_areas" D:\eda\ices\ICES_areas -overwrite -nlt MULTIPOLYGON -progress --config PG_USE_COPY YES
+ogr2ogr.exe -f "PostgreSQL" PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="ices_areas" D:\eda\ices\ICES_areas -nlt MULTIPOLYGON -overwrite  -progress --config PG_USE_COPY YES
 
-ogr2ogr.exe -f "PostgreSQL" -a_srs EPSG:4326 PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="ices_ecoregions" D:\eda\ices\ICES_ecoregions -overwrite -progress --config PG_USE_COPY YES
+ogr2ogr.exe -f "PostgreSQL" PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="ices_ecoregions" D:\eda\ices\ICES_ecoregions -nlt MULTIPOLYGON -overwrite -progress --config PG_USE_COPY YES
