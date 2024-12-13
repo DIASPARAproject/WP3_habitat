8c4d480a2fdc69a01783578ce953f6d11787efa1
olivierojules <oliviero.jules@gmail.com>
2024-12-13 10:45:24 +0100

    dev: swaping source for country layer

--- bash/0_powershell_import_countries.sh
+++ bash/0_powershell_import_countries.sh
@@ -4,7 +4,7 @@
 $pathccmsource = "D:\eda\countries"
 $pathccmout = "D:\eda\countries"
 
-$namefile = "country_shapes"
+$namefile = "ne_10m_admin_0_countries"
 cd $pathccmsource
-curl -o "$namefile.zip" "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/country_shapes/exports/shp?lang=fr&timezone=Europe%2FBerlin"
+curl -o "$namefile.zip" "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/ne_10m_admin_0_countries/exports/shp?lang=fr&timezone=Europe%2FBerlin"
 Expand-Archive "$namefile.zip" -DestinationPath "$pathccmout"
--- bash/1_osgeo4w_ogr2ogr_gdb_postgres_countries.bash
+++ bash/1_osgeo4w_ogr2ogr_gdb_postgres_countries.bash
@@ -1,3 +1,3 @@
 # launch in OSGEO4W console, (QGIS) line by line
 
-ogr2ogr.exe -f "PostgreSQL" -a_srs EPSG:4326 PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="tempo" D:\eda\countries -overwrite -progress --config PG_USE_COPY YES
+ogr2ogr.exe -f "PostgreSQL" -a_srs EPSG:4326 PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="tempo" D:\eda\countries -nlt MULTIPOLYGON  -overwrite -progress --config PG_USE_COPY YES
