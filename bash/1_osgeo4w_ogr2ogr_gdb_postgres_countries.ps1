940ae9d41a059b3f444a2ee03af3c06701e2d973
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-01-14 13:34:36 +0100

    dev : rename column #4

--- bash/1_osgeo4w_ogr2ogr_gdb_postgres_countries.ps1
+++ bash/1_osgeo4w_ogr2ogr_gdb_postgres_countries.ps1
@@ -1,3 +1,5 @@
 # launch in OSGEO4W console, (QGIS) line by line
 
 ogr2ogr.exe -f "PostgreSQL" -a_srs EPSG:4326 PG:"host=localhost port=5432 dbname=diaspara user=postgres password=postgres" -lco SCHEMA="tempo" D:\eda\countries -nlt MULTIPOLYGON  -overwrite -progress --config PG_USE_COPY YES
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@$env:hostdiaspara/diaspara -c "ALTER TABLE tempo.ne_10m_admin_0_countries rename COLUMN wkb_geometry  to geom;" 
