216a54d686d7ff00f10e3174403e264de05b94aa
olivierojules <oliviero.jules@gmail.com>
2025-03-11 16:35:26 +0100

    dev: sql dump of the hydroshed database #4 #7 #13

---
+++ bash/99_dump_hydroshed_db.ps1
@@ -0,0 +1,23 @@
+# run in powershell
+
+pg_dump -U postgres -F custom -d diaspara -n h_adriatic > h_adriatic.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_baltic22to26 > h_baltic22to26.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_baltic27to29_32 > h_baltic27to29_32.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_baltic30to31 > h_baltic30to31.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_barent > h_barent.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_biscayiberian > h_biscayiberian.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_blacksea > h_blacksea.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_celtic > h_celtic.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_iceland > h_iceland.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_medcentral > h_medcentral.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_medeast > h_medeast.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_medwest > h_medwest.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_norwegian > h_norwegian.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_nseanorth > h_nseanorth.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_nseasouth > h_nseasouth.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_nseauk > h_nseauk.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_southatlantic > h_southatlantic.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_southmedcentral > h_southmedcentral.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_southmedeast > h_southmedeast.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_southmedwest > h_southmedwest.pgc
+pg_dump -U postgres -F custom -d diaspara -n h_svalbard > h_svalbard.pgc
