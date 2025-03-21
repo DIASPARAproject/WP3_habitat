2e068cba491c41a4652eb2d0770432b335fa4825
Jani Helminen <54444266+Nojani@users.noreply.github.com>
2025-03-21 16:34:38 +0100

    downloading data

--- bash/99_exchanges_with_jani.ps1
+++ bash/99_exchanges_with_jani.ps1
@@ -13,18 +13,60 @@ $pathsql = "C:\Users\03224501\OneDrive - Valtion\GitHub\WP3_habitat\SQL\create_r
 psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -f $pathsql
 
 # delete the schema first before transferring the same named schema. NOTE: cascade if there is a vue or anything else inside
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_baltic22to26 cascade"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_baltic27to29_32 cascade"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_baltic30to31 cascade"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_adriatic cascade"
 psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_barents cascade"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_biscayiberian cascade"
+
 
 # testing the dump of just one schema
-
 pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_barents -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
 
-#save the distantschema locally and transfer to local db. as compressed file
+#OR OTHER OPTION save the distantschema locally and transfer to local db. as compressed file
 cd "C:\Users\03224501\Downloads"
 pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_barents -v -Fc -f "h_barents.backup"
-
 #because it was compressed, we need to restore, instead of psql
 pg_restore --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara "h_barents.backup"
+
+
+#Get all Schemas
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema ref -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_adriatic -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_baltic22to26 -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_baltic27to29_32 -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_baltic30to31 -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_biscayiberian -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+
+
+$schemas = @(
+    "h_blacksea",
+    "h_celtic",
+    "h_iceland",
+    "h_medcentral",
+    "h_medeast",
+    "h_medwest",
+    "h_norwegian",
+    "h_nseanorth",
+    "h_nseasouth",
+    "h_nseauk",
+    "h_southatlantic",
+    "h_southmedcentral",
+    "h_southmedeast",
+    "h_southmedwest",
+    "h_svalbard",
+    "janis",
+    "refbast",
+    "tempo"
+)
+foreach ($schema in $schemas) {
+    pg_dump --dbname="postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara" --schema $schema -v | psql --dbname="postgresql://${env:userlocal}:${env:passlocal}@127.0.0.1/diaspara"
+}
+
+
+
+
 
 psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@$env:hostdiaspara/postgres -c "DROP DATABASE diaspara"
 cd "C:\Users\cedric.briand\OneDrive - EPTB Vilaine\partage\diaspara"
