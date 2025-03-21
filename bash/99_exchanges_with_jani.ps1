eb58d3d4219391c8896a81ee4cbd87e1ce077593
Jani Helminen <54444266+Nojani@users.noreply.github.com>
2025-03-21 13:57:16 +0100

    working on saving

--- bash/99_exchanges_with_jani.ps1
+++ bash/99_exchanges_with_jani.ps1
@@ -8,12 +8,23 @@
 #first create the database
 CREATEDB -U ${env:userlocal} diaspara 
 # second step set the users in the server (set the right path for you)
-$pathsql = "C:\workspace\WP3_habitat\SQL\create_role_diaspara.sql"
+$pathsql = "C:\Users\03224501\OneDrive - Valtion\GitHub\WP3_habitat\SQL\create_role_diaspara.sql"
 
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -f $pathsql
 
-# testing the dump of just one table
+# delete the schema first before transferring the same named schema. NOTE: cascade if there is a vue or anything else inside
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara -c "drop schema h_barents cascade"
 
-pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_barent -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+# testing the dump of just one schema
+
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_barents -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara
+
+#save the distantschema locally and transfer to local db. as compressed file
+cd "C:\Users\03224501\Downloads"
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostdiaspara}/diaspara --schema h_barents -v -Fc -f "h_barents.backup"
+
+#because it was compressed, we need to restore, instead of psql
+pg_restore --dbname=postgresql://${env:userlocal}:${env:passlocal}@${127.0.0.1}/diaspara "h_barents.backup"
 
 psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@$env:hostdiaspara/postgres -c "DROP DATABASE diaspara"
 cd "C:\Users\cedric.briand\OneDrive - EPTB Vilaine\partage\diaspara"
