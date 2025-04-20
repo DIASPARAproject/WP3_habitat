46c0bb0479120ead9f7e0ca20e5e79295f465326
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-04-20 16:57:50 +0200

    dev : dumping to the server

--- bash/99_exchange_with_jules.ps1
+++ bash/99_exchange_with_jules.ps1
@@ -26,3 +26,61 @@ pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@$env:hostdiaspar
 
 # Dump to server
 pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --schema h_barent -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+
+# cedric 20/04/2024. Dumping work from localhost to server
+# ref
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_lifestage_lfs CASCADE;"
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_species_spe CASCADE;"
+# will drop ref.tr_metadata_met
+# will drop ref.tr_version_ver
+# will drop refnas.tr_version_ver
+# will drop refnas. tr_metadata_met
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_maturity_mat CASCADE;"
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_quality_qal CASCADE;"
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_metadata_met CASCADE;"
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_version_ver CASCADE;"
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_lifestage_lfs CASCADE;"
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_species_spe CASCADE;"
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_category_cat CASCADE;"
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists ref.tr_destination_des CASCADE;"
+
+
+# Dump to server
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table ref.tr_lifestage_lfs --table ref.tr_maturity_mat --table ref.tr_quality_qal --table ref.tr_version_ver --table  ref.tr_species_spe --table ref.tr_destination_des --table ref.tr_category_cat -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table ref.tr_metadata_met -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists refnas.tr_metadata_met CASCADE;"
+
+psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara -c "DROP TABLE if exists refnas.tr_version_ver CASCADE;"
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table refnas.tr_version_ver --table refnas.tr_metadata_met -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+
+# For Jani and Jules to dump to localhost (normally you don't have these tables
+# or the version needs replacement.)
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_maturity_mat CASCADE;"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_quality_qal CASCADE;"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_metadata_met CASCADE;"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_version_ver CASCADE;"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_lifestage_lfs CASCADE;"
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_species_spe CASCADE;"
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_category_cat CASCADE;"
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists ref.tr_destination_des CASCADE;"
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists refnas.tr_metadata_met CASCADE;"
+
+psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara -c "DROP TABLE if exists refnas.tr_version_ver CASCADE;"
+
+
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara  --table ref.tr_lifestage_lfs --table ref.tr_maturity_mat --table ref.tr_quality_qal --table ref.tr_version_ver --table  ref.tr_species_spe --table ref.tr_destination_des --table ref.tr_category_cat --table ref.tr_metadata_met -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara
+
+pg_dump --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara --table refnas.tr_version_ver --table refnas.tr_metadata_met -v | psql --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara
